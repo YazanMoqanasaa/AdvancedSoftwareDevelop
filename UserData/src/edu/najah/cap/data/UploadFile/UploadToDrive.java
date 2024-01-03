65a9d84c2712bcf383bab4ae25414d2e5ac0134d
Mohammad Soqy <96316121+Mohamadsoqy1@users.noreply.github.com>
2024-01-03 21:33:31 +0200

    Upload File to Google Drive using Singleton

--- UserData/src/edu/najah/cap/data/UploadFile/UploadToDrive.java
+++ UserData/src/edu/najah/cap/data/UploadFile/UploadToDrive.java
@@ -11,12 +11,23 @@ import com.google.api.services.drive.model.File;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.Collections;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
 public class UploadToDrive {
     private static final String APPLICATION_NAME = "Upload File";
+    private static final Logger logger = Logger.getLogger(UploadToDrive.class.getName());
     private static UploadToDrive instance;
     private Drive service;
+
     private UploadToDrive(String serviceAccountJsonPath) {
         try {
+            // Configure logging
+            configureLogger();
+
             HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
             JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
 
@@ -29,15 +40,29 @@ public class UploadToDrive {
                     .build();
 
         } catch (GeneralSecurityException | IOException e) {
-            e.printStackTrace();
+            logger.log(Level.SEVERE, "Error initializing Drive service", e);
         }
     }
+
+    private void configureLogger() {
+        try {
+            // Create file handler for logging
+            Handler fileHandler = new FileHandler("application.log");
+            fileHandler.setFormatter(new SimpleFormatter());
+            logger.addHandler(fileHandler);
+            logger.setLevel(Level.ALL);
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Error configuring logger", e);
+        }
+    }
+
     public static UploadToDrive getInstance(String serviceAccountJsonPath) {
         if (instance == null) {
             instance = new UploadToDrive(serviceAccountJsonPath);
         }
         return instance;
     }
+
     public String uploadFile(String filePath, String fileName, String parentFolderId) {
         try {
             // Set metadata for the file
@@ -54,9 +79,13 @@ public class UploadToDrive {
                     .setFields("id")
                     .execute();
 
+            // Log successful file upload
+            logger.info("File uploaded successfully. File ID: " + uploadedFile.getId());
+
             return uploadedFile.getId();
         } catch (IOException e) {
-            e.printStackTrace();
+            // Log error during file upload
+            logger.log(Level.SEVERE, "Error uploading file", e);
             return null;
         }
     }
